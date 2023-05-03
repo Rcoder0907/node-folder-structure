eacaeb91e3e1c0374485154d03170e5b9d0ccc03
rcoder <rcoder0907@gmail.com>
2023-05-03 21:56:35 +0530

    Project setup

---
+++ db/index.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql2');
+const fs = require('fs');
+const bcrypt = require('bcryptjs');
+
+require('dotenv').config();
+
+// Read SQL seed query
+const seedQuery = fs.readFileSync('db/seed.sql', {
+	encoding: 'utf-8'
+});
+
+// Connect to database
+const connection = mysql.createConnection({
+	host: process.env.TYPEORM_HOST,
+	user: process.env.TYPEORM_USERNAME,
+	password: process.env.TYPEORM_PASSWORD,
+	database: process.env.TYPEORM_DATABASE,
+	multipleStatements: true
+});
+
+connection.connect();
+
+// Generate random password for initial admin user
+const psw = Math.random().toString(36).substring(2);
+const hash = bcrypt.hashSync(psw, 10);
+
+console.log('Running SQL seed...');
+
+// Run seed query
+connection.query(seedQuery, [hash], (err) => {
+	if (err) {
+		throw err;
+	}
+
+	console.log('SQL seed completed! Password for initial admin account: ' + psw);
+	connection.end();
+});
